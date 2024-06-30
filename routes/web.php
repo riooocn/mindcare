8a46ff0965331c4a13c8474c3ba60dac187d5b0c
Gorthdzx <krislaksana123@gmail.com>
2024-06-30 12:43:37 +0700

    chore : tambah rute article

--- routes/web.php
+++ routes/web.php
@@ -4,5 +4,10 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     $title = 'Home';
-    return view('home',['title'=>$title]);
+    return view('home', ['title' => $title]);
+});
+
+Route::get('/article', function () {
+    $title = "Article";
+    return view('home', ['title' => $title]);
 });
